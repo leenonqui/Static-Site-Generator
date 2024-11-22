3c8a629b4225356d15c8229594aa6765a8eec41a
leenonqui <onjo2001@gmail.com>
2024-11-22 16:24:12 +0100

    update htmlnode.py and test_htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,10 +9,28 @@ class HTMLNode():
         raise NotImplementedError
 
     def props_to_html(self):
-        if self.props == None:
-           return ''
+        if self.props is None:
+            return ''
         list = map(lambda key: f'{key}="{self.props[key]}"', self.props)
         return ' ' + ' '.join(list)
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        else:
+            if self.props is None:
+                return f'<{self.tag}>{self.value}</{self.tag}>'
+            return f'<{self.tag}{super().props_to_html()}>{self.value}</{self.tag}>'
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
+
 
 class TestHTMLNode(unittest.TestCase):
     def test_no_props(self):
@@ -12,7 +13,7 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_one_property(self):
         node = HTMLNode(
-            props={"class":"header"}
+            props={"class": "header"}
         )
         expected_output = ' class="header"'
         self.assertEqual(
@@ -54,15 +55,84 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_repr(self):
         node = HTMLNode(
-                    "p",
-                    "What a strange world",
-                    None,
-                    {"class": "primary"},
-                )
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
         self.assertEqual(
             node.__repr__(),
             "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})"
         )
+
+    def test_leafnode(self):
+        node = LeafNode(
+            "p",
+            "What a strange test",
+            {"href": "https://www.google.com"}
+        )
+        node2 = LeafNode(
+            None,
+            "What a strange test"
+        )
+        self.assertEqual(
+            node.tag,
+            "p"
+        )
+        self.assertEqual(
+            node.value,
+            "What a strange test"
+        )
+        self.assertEqual(
+            node.props,
+            {"href": "https://www.google.com"}
+        )
+
+        self.assertEqual(
+            node2.tag,
+            None
+        )
+        self.assertEqual(
+            node2.value,
+            "What a strange test"
+        )
+        self.assertEqual(
+            node2.props,
+            None
+        )
+
+    def test_leaf_to_html(self):
+        node = LeafNode(
+            None,
+            "What a strange test"
+        )
+        node2 = LeafNode(
+            "p",
+            "This is a paragraph of text."
+        )
+        node3 = LeafNode(
+            "a",
+            "Click me!",
+            {"href": "https://www.google.com"}
+        )
+
+        self.assertEqual(
+            node.to_html(),
+            "What a strange test"
+        )
+        self.assertEqual(
+            node2.to_html(),
+            "<p>This is a paragraph of text.</p>"
+        )
+        self.assertEqual(
+            node3.to_html(),
+            '<a href="https://www.google.com">Click me!</a>'
+        )
+
+    def test_value_error_to_html(self):
+        with self.assertRaises(ValueError):
+            LeafNode("p", None).to_html()
+
 
 if __name__ == "__main__":
     unittest.main()
