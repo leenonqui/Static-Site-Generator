a59149e3619f2460f76c843b39926bc00e73da4b
leenonqui <onjo2001@gmail.com>
2024-12-08 17:15:18 +0100

    fix bug in block_markdown.py and add test in test_block_markdown.py

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,6 +1,4 @@
-# imports
 
 
-# code
 def markdown_to_blocks (doc: str) -> list[str]:
-    return [item.strip(' ') for item in list(doc.split('\n\n'))] or []
+    return [item.strip() for item in list(doc.split('\n\n')) if item != ''] or []
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -5,14 +5,45 @@ from block_markdown import markdown_to_blocks
 
 class TestInlineMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
-        doc = "# This is a heading \n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item "
-        expected_output = [
+        md = """
+# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+"""
+        blocks = [
             '# This is a heading',
             'This is a paragraph of text. It has some **bold** and *italic* words inside of it.',
             '* This is the first list item in a list block\n* This is a list item\n* This is another list item'
         ]
 
         self.assertEqual(
-            markdown_to_blocks(doc),
-            expected_output
+            markdown_to_blocks(md),
+            blocks
         )
+
+    def test_multiple_newlines(self):
+        md = """
+# This is a heading
+
+
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+"""
+        blocks = [
+            "# This is a heading",
+            "This is a paragraph of text. It has some **bold** and *italic* words inside of it."
+        ]
+
+        self.assertEqual(
+            markdown_to_blocks(md),
+            blocks
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
